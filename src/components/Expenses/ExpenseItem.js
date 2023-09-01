3779baf1fb98b5af166084cc39b1136fcf71f06d
prathamkumarjha <prathamjha123dcs@gmail.com>
2023-09-01 17:19:27 +0530

    added expense chart and final touches to the project

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -19,7 +19,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>₹{props.amount}</div>
       </div>
       {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
